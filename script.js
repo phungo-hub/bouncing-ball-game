27b60199302b1441d1f2e78a55e2720a81248315
phungo-hub <ngokieuphu@gmail.com>
2022-09-30 11:00:08 +0700

    update file script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext("2d");
 let scoreboard = document.getElementById('scoreboard');
+let start = document.getElementById("start");
 
 const GAME_BOARD_WIDTH = 480, GAME_BOARD_HEIGHT = 360;
-const BALL_RADIUS = 7, BALL_RADIAN = 30, BALL_SPEED = 10;
+const BALL_RADIUS = 7, BALL_RADIAN = 30, GAME_RENDER_SPEED = 10;
 
 let x = canvas.width/2;
 let y = canvas.height - BALL_RADIAN;
@@ -21,20 +22,25 @@ let leftPressed = false;
 let GameBoard = function(width, height) {
     this.width = width;
     this.height = height;
+    this.start = false;
     this.drawGameBoard = function(canvas) {
         canvas.setAttribute('width', this.width);
         canvas.setAttribute('height', this.height);
         x = canvas.width/2;
         y = canvas.height - BALL_RADIAN;
     }
-
+    start.addEventListener("click", function() {
+        this.start = true;
+        if (this.start) {
+            canvas.style.visibility ="visible";
+            scoreboard.style.visibility = "visible";
+            start.style.visibility = "hidden";
+        }
+    })
 }
 
 let Ball = function() {
-    this.xCoordinate = x;
     this.yCoordinate = y;
-    this.ballRadian = BALL_RADIAN;
-    this.ballSpeed = BALL_SPEED;
 
     this.move = function() {
         if (x + dx > canvas.width - BALL_RADIUS || x + dx < BALL_RADIUS) {
@@ -47,10 +53,11 @@ let Ball = function() {
             {
                 dy = -dy;
             }
-        } else if (y + BALL_RADIUS > canvas.height - 40 -PADDLE_HEIGHT) {
+        }  else if (y + BALL_RADIUS > canvas.height - 40 -PADDLE_HEIGHT) {
             if (y + dy - 5 >= canvas.height - BALL_RADIUS) {
                 alert("HAHAHA. Gà \nĐiểm: " + score );
                 clearInterval(interval);
+                clearInterval(scoreInterval);
                 document.location.reload();
             }
         }
@@ -60,7 +67,7 @@ let Ball = function() {
     this.drawBall = function() {
         ctx.beginPath();
         ctx.arc(x, y, BALL_RADIUS, 0, Math.PI*2);
-        ctx.fillStyle = "#7c7c7c";
+        ctx.fillStyle = "#779ecb";
         ctx.fill();
         ctx.closePath();
     }
@@ -68,12 +75,12 @@ let Ball = function() {
 let Paddle = function(width, xCoordinate) {
     this.width = width;
     this.height = PADDLE_HEIGHT;
-    this.xCoordinate = xCoordinate;
     this.yCoordinate = canvas.height - 40 -PADDLE_HEIGHT;
 
     this.move = function() {
         document.addEventListener("keydown", keyDownHandler, false);
         document.addEventListener("keyup", keyUpHandler, false);
+        document.addEventListener("mousemove", mouseMoveHandler, false)
 
         function keyDownHandler(e) {
             if (e.key === "Right" || e.key === "ArrowRight") {
@@ -90,6 +97,19 @@ let Paddle = function(width, xCoordinate) {
                 leftPressed = false;
             }
         }
+
+        function mouseMoveHandler(e) {
+            const relativeX = e.clientX - canvas.offsetLeft;
+            if (relativeX > 0 && relativeX < canvas.width) {
+                paddleX = relativeX - PADDLE_WIDTH / 2;
+                if (paddleX <= 0) {
+                    paddleX = 0;
+                } else if (paddleX >= canvas.width - PADDLE_WIDTH) {
+                    paddleX = canvas.width - PADDLE_WIDTH;
+                }
+            }
+        }
+
         if (rightPressed) {
             paddleX += 7;
             if (paddleX + PADDLE_WIDTH > canvas.width) {
@@ -107,14 +127,14 @@ let Paddle = function(width, xCoordinate) {
     this.drawPaddle = function() {
         ctx.beginPath();
         ctx.rect(paddleX, this.yCoordinate, this.width, this.height);
-        ctx.fillStyle = "blue";
+        ctx.fillStyle = "#FFC0CB";
         ctx.fill();
         ctx.closePath();
     }
 }
 
 function drawGame() {
-    let ball = new Ball(x, y, BALL_RADIAN, BALL_SPEED);
+    let ball = new Ball(x, y, BALL_RADIAN, GAME_RENDER_SPEED);
     let paddle = new Paddle(PADDLE_WIDTH, 200);
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -127,9 +147,9 @@ function drawGame() {
 
 let gameBoard = new GameBoard(GAME_BOARD_WIDTH, GAME_BOARD_HEIGHT);
 gameBoard.drawGameBoard(canvas);
-const interval = setInterval(drawGame, BALL_SPEED);
+const interval = setInterval(drawGame, 10);
 
-setInterval(function() {
+const scoreInterval = setInterval(function() {
     score++;
     scoreboard.innerHTML = "Score: " + score;
 }, 1000);
